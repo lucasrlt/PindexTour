512838f8610162644fb3694a6bd2618db1661f6b
Lucas Rollet <lucas.rollet.69@gmail.com>
2021-03-09 15:18:20 +0100

    add tests

--- app/src/test/java/com/pindex/main/utils/ConstantsTest.kt
+++ app/src/test/java/com/pindex/main/utils/ConstantsTest.kt
@@ -1,4 +1,7 @@
 package com.pindex.main.utils
+import android.widget.ImageView
+import com.pindex.main.MainApplication
+import com.pindex.main.di.ExperienceRepositoryDI
 import junit.framework.TestCase.assertNotNull
 import org.junit.Test
 
@@ -9,6 +12,15 @@ class ConstantsTest {
         assertNotNull(Constants)
         assertNotNull(Constants.BlockFragmentArguments)
         assertNotNull(Constants.BlocksTypes)
+
+        assertNotNull(MainApplication());
+        assertNotNull(ExperienceRepositoryDI());
+    }
+
+    @Test
+    fun testMediaLoader() {
+        assertNotNull(MediaLoader);
+
     }
 
 }
